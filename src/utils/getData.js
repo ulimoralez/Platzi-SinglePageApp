41546f9cb61d79c8a426eaceb4a4b69afe18b815
Ulises Moralez <moralezulises@gmail.com>
2021-08-21 00:27:27 -0300

    Typo

--- src/utils/getData.js
+++ src/utils/getData.js
@@ -1,15 +1,14 @@
-const API = 'https://rickandmortyapi.com/api/character';
+const API = 'https://rickandmortyapi.com/api/character/';
 
 const getData = async (id) => {
-    const apiURL = id ? '${API}${id}' : API;
-
+    const apiURl = id ? `${API}${id}` : API;
     try {
-        const response = await fetch(apiURL);
+        const response = await fetch(apiURl);
         const data = await response.json();
         return data;
     } catch (error) {
         console.log('Fetch Error', error);
-    }
-}
+    };
+};
 
 export default getData;
